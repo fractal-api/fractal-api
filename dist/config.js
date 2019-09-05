426df25878bd704ea00bc996dc3268f4f09a76a9
Jonny Lea <jonny@fractal-labs.com>
2019-09-05 18:41:36 +0100

    remove build folder

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiDomain = "apis.fractal-dev.co.uk";
-exports.bankUrl = "https://" + exports.apiDomain + "/banking";
-exports.companyUrl = "https://" + exports.apiDomain + "/companies";
-exports.forecastsUrl = "https://" + exports.apiDomain + "/forecasts";
-exports.categoriesUrl = "https://" + exports.apiDomain + "/categories";
-exports.authUrl = "https://" + exports.apiDomain + "/token";
